33ea3f12d08db1d6de51311cef33e9c62e82e3fd
Renato <rntdek@gmail.com>
2020-11-03 17:42:16 -0300

    migrations

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+interface Request {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    public async execute({ date, provider }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+        const appointmentDate = startOfHour(date);     
+
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+
+        if(findAppointmentInSameDate) {
+            throw Error('This appointment is alreay booked');
+        }
+
+        const appointment = appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
