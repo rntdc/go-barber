fa76e0ef4c8e8627204754675a60040af1d4b35c
Renato <rntdek@gmail.com>
2020-11-11 09:27:32 -0300

    upload de arquivo

--- src/database/migrations/1605095677539-AddAvatarFieldToUsers.ts
+++ src/database/migrations/1605095677539-AddAvatarFieldToUsers.ts
@@ -1,11 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class AddAvatarFieldToUsers1605095677539 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+        await queryRunner.addColumn(
+          'users',
+          new TableColumn({
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          }),
+        );
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('users', 'avatar');
+      }
 
 }
